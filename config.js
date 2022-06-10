401bf694b369c91f9e3d480a92b1d768ce2da3e8
Walter Miani <walter.miani@gmail.com>
2022-06-10 17:03:37 +0200

    primo

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+    module.exports = {
+        bot_token : process.env.BOT_TOKEN,
+        id: process.env.ID,
+        port : process.env.PORT,
+    }
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const {Telegraf} = require('telegraf')
+const {bot_token, port, id} = require('./config')
+
+app.use(require('helmet')())
+
+const bot = new Telegraf(bot_token)
+
+app.set('port', process.env.PORT || port)
+bot.start((ctx) => ctx.reply('Welcome human!'))
+
+app.use(bot.webhookCallback(`/${bot_token}`))
+bot.telegram.setWebhook(`https://telegram-nfc.herokuapp.com/${bot_token}`||`https://8a56230c.ngrok.io/${bot_token}`)
+
+app.get(`/${bot_token}`, async (req,res)=> {
+    if (req.query.message) {
+        try {
+            await bot.telegram.sendMessage(id, req.query.message)    
+            res.send('Message Sended!')
+        } catch (error) {
+            res.send(error)    
+        }
+    }else {
+        res.send('No message to send')
+    }
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+})

830e0684ea01a2a0067243fc3472c43e123ec1b3
Walter Miani <walter.miani@gmail.com>
2022-06-10 18:30:58 +0200

    sistemato coffee

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const bot = new Telegraf(bot_token)
 app.set('port', process.env.PORT || port)
 bot.start((ctx) => ctx.reply('Welcome human!'))
 
-var id = '16885275'
-// var id = '-780148931'
+// var id = '16885275'
+var id = '-780148931'
 
 // bot.telegram.sendMessage(id, 'Bene bravi tutti. Buon weekend.') 
 
@@ -19,16 +19,12 @@ var id = '16885275'
 bot.telegram.setWebhook(`https://telegram-nfc.herokuapp.com/${bot_token}`||`https://8a56230c.ngrok.io/${bot_token}`)
 
 app.get(`/coffee`, async (req,res)=> {
-    if (req.query.message) {
         try {
             await bot.telegram.sendMessage(id, 'Coffee?') 
-            res.send('Message Sended!')
+            res.send('Message Sent!')
         } catch (error) {
             res.send(error)    
         }
-    }else {
-        res.send('No message to send')
-    }
 })
 
 app.listen(app.get('port'), () => {

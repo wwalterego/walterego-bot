4bd17c8e03a5b93ae6862d30555f0f1077e4f509
Walter Miani <walter.miani@gmail.com>
2022-06-10 18:23:05 +0200

    facilitata api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const Telegraf = require('telegraf')
-const {bot_token, port, id} = require('./config')
+const {Telegraf} = require('telegraf')
+const {bot_token, port} = require('./config')
 
 app.use(require('helmet')())
 
@@ -10,13 +10,18 @@ const bot = new Telegraf(bot_token)
 app.set('port', process.env.PORT || port)
 bot.start((ctx) => ctx.reply('Welcome human!'))
 
-app.use(bot.webhookCallback(`/${bot_token}`))
+var id = '16885275'
+// var id = '-780148931'
+
+// bot.telegram.sendMessage(id, 'Bene bravi tutti. Buon weekend.') 
+
+// app.use(bot.webhookCallback(`/${bot_token}`))
 bot.telegram.setWebhook(`https://telegram-nfc.herokuapp.com/${bot_token}`||`https://8a56230c.ngrok.io/${bot_token}`)
 
-app.get(`/${bot_token}`, async (req,res)=> {
+app.get(`/coffee`, async (req,res)=> {
     if (req.query.message) {
         try {
-            await bot.telegram.sendMessage(id, req.query.message)    
+            await bot.telegram.sendMessage(id, 'Coffee?') 
             res.send('Message Sended!')
         } catch (error) {
             res.send(error)    

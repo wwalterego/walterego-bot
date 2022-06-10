9db2ec843a9f8bd7303f7997f58b3b5f61a320de
Walter Miani <walter.miani@gmail.com>
2022-06-10 17:13:15 +0200

    sistemate dipendenze

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {Telegraf} = require('telegraf')
+const Telegraf = require('telegraf')
 const {bot_token, port, id} = require('./config')
 
 app.use(require('helmet')())
